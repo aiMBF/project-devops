c1787bdbc04aebb052d2b55a7700e047d8b65505
aiMBF <aissasaykou@gmail.com>
2023-09-01 06:24:37 +0000

    update settings

--- devopsPproject/settings.py
+++ devopsPproject/settings.py
@@ -91,11 +91,10 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
+        "HOST": "localhost",  # set in docker-compose.yml
         "PORT": 5432,  # default postgres port
         'TEST': {
             'NAME': "postgres_test", 
-            'HOST': "localhost",
         }
     }
 }
